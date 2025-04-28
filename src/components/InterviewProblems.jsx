42e10e7295f2b4b231f269d9619396e3c8c0d958
DONTHIREDDY LOKESH REDDY <lokeshreddydonthireddy28.com>
2025-04-28 19:51:03 +0530

    new

--- src/components/InterviewProblems.jsx
+++ src/components/InterviewProblems.jsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { useState } from "react"
 import { ExternalLink, ChevronDown, ChevronUp } from "lucide-react"
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { useState } from "react"
 import { Github, ExternalLink, X } from "lucide-react"
 
